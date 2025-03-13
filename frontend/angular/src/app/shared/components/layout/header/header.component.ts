ffeae6643680edd26b3d7ea857851e3936a6bd6e
Llorenç Alfonso Sanchis <163330679+alfosan@users.noreply.github.com>
2025-03-13 11:32:56 +0100

    Admin --> Angular --> Header Component Guard show

--- frontend/angular/src/app/shared/components/layout/header/header.component.ts
+++ frontend/angular/src/app/shared/components/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TokenService } from '../../../../core/services/token/token.service';
@@ -8,49 +8,49 @@ import { User } from '../../../../core/models/Users/user.model';
 import { NotificationsListComponent } from '../../bell/notifications-list/notifications-list.component';
 
 @Component({
-    selector: 'app-header',
-    standalone: true,
-    imports: [RouterModule, CommonModule, NotificationsListComponent],
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule, CommonModule, NotificationsListComponent],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-    menuOpen = false;
-    routes = SHARED_ROUTES;
-    profileMenuOpen = false;
-    notificationsOpen = false;
-    user!: User;
-    
-    constructor(
-        public tokenService: TokenService,
-        private userService: UserService,
-    ) {}
-    
-    
-    ngOnInit() {
-        if (this.tokenService.isAuthenticated()) {
-            const userInfo = this.tokenService.getUserInfo();
-            if (userInfo) {
-                this.user = userInfo;
-            }
-        }
-    }
+export class HeaderComponent implements OnInit {
+  menuOpen = false;
+  routes = SHARED_ROUTES;
+  profileMenuOpen = false;
+  notificationsOpen = false;
+  user!: User;
 
-    toggleProfileMenu() {
-        this.profileMenuOpen = !this.profileMenuOpen;
-    }
+  constructor(
+    public tokenService: TokenService,
+    private userService: UserService,
+  ) {}
 
-    toggleNotifications() {
-        this.notificationsOpen = !this.notificationsOpen;
+  ngOnInit() {
+    if (this.tokenService.isAuthenticated()) {
+      const userInfo = this.tokenService.getUserInfo();
+      if (userInfo) {
+        this.user = userInfo;
+      }
     }
+  }
 
-    toggleMenu() {
-        this.menuOpen = !this.menuOpen;
-    }
+  toggleProfileMenu() {
+    this.profileMenuOpen = !this.profileMenuOpen;
+  }
 
-    logout() {
-        this.userService.logout();
-        this.tokenService.clearAll();
-        window.location.href = this.routes.NEXT.HOME;
-    }
+  toggleNotifications() {
+    this.notificationsOpen = !this.notificationsOpen;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  logout() {
+    this.userService.logout().subscribe(() => {
+      this.tokenService.clearAll();
+      window.location.href = this.routes.NEXT.HOME;
+    });
+  }
 }
